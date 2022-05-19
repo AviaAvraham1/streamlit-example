6386dda973faf922630afceab02eb039285ff5c3
Avia Avraham <avia.avraham@campus.technion.ac.il>
2022-05-19 23:58:32 +0300

    added sound

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 from collections import namedtuple
+import os
+from time import sleep
 import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from gtts import gTTS
+import base64
+#from script.py import foo 
 
 """
 # Welcome to Streamlit!
@@ -15,29 +20,30 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+
+myobj = gTTS(text="Bak Bouk", lang='en', slow=True)
+myobj.save("welcome.mp3")
+
+audio_file = open('welcome.mp3', 'rb')
+audio_bytes = audio_file.read()
+
+#st.audio(audio_bytes, format='audio/mp3')
+
+mymidia_placeholder = st.empty()
+
+mymidia_str = "data:audio/ogg;base64,%s"%(base64.b64encode(audio_bytes).decode())
+mymidia_html = """
+                <audio autoplay class="stAudio">
+                <source src="%s" type="audio/ogg">
+                Your browser does not support the audio element.
+                </audio>
+            """%mymidia_str
+
+mymidia_placeholder.empty()
+sleep(1)
+mymidia_placeholder.markdown(mymidia_html, unsafe_allow_html=True)
+
 picture = st.camera_input("Take a picture")
 
 if picture:
      st.image(picture)
-
-     
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
