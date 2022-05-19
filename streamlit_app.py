78f809c6d1c69e485b2b1a14f26c9b36607f5778
Avia Avraham <avia.avraham@campus.technion.ac.il>
2022-05-19 21:10:12 +0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,12 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+picture = st.camera_input("Take a picture")
 
+if picture:
+     st.image(picture)
+
+     
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
