e52bf0703efd31e413176a16e3ed45bcdebb5266
Avia Avraham <avia.avraham@campus.technion.ac.il>
2022-05-20 15:15:52 +0300

    an attempt

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,16 +19,7 @@ from io import BytesIO
 import gzip
 import xml.etree.ElementTree as ET
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
 
 #myobj = gTTS(text="Bak Bouk", lang='en', slow=True)
